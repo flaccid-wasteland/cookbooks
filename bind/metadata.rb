0b5b70a6f0ea989715edecb58f293e9bb8e01893
Chris Fordham <chris@xhost.com.au>
2010-10-04 10:36:26 +1100

    Add bind blank recipe

---
+++ bind/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Chris Fordham"
+maintainer_email "chris@xhost.com.au"
+license          "Apache 2.0"
+description      "Installs/Configures bind"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
