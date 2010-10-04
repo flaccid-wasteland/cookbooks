3ebba70878a5f484ca43a36e7d386329c99902ba
Chris Fordham <chris@xhost.com.au>
2010-10-05 02:55:48 +1100

    Added env cookbook

---
+++ cookbooks/env/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Chris Fordham"
+maintainer_email "chris@xhost.com.au"
+license          "Apache 2.0"
+description      "Displays shell environment of Recipes"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+
+recipe "env::default", "Shows shell environment of Chef Recipes on this platform."
