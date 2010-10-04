bc4ea76e79db0cb10829464a7424c6d08a05014e
Chris Fordham <chris@xhost.com.au>
2010-10-05 00:45:52 +1100

    Add recipies to metadata.rb

--- cookbooks/rs_ebs/metadata.rb
+++ cookbooks/rs_ebs/metadata.rb
@@ -4,3 +4,6 @@ license          "Apache 2.0"
 description      "Installs/Configures rs_ebs"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+
+recipe "rs_ebs::default", "Installs RightScale EBS Tools"
+recipe "rs_ebs::tools_install", "Installs RightScale EBS Tools"
