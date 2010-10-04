848b6e148f69a2e4dc07a7a84695de29d8b34c38
Chris Fordham <chris@xhost.com.au>
2010-10-05 02:24:08 +1100

    Updated metadata

--- cookbooks/rs_ebs/metadata.rb
+++ cookbooks/rs_ebs/metadata.rb
@@ -12,19 +12,19 @@ recipe "rs_ebs::volume_restore", "Restores a new EBS volume from an EBS snapshot
 attribute "ebs/restore_fs_type",
   :display_name => "EBS volume restore filesystem type",
   :description => "The EBS volume filestytem type",
-  :default => nil
+  :default => "xfs"
 
 attribute "ebs/restore_mount_point",
   :display_name => "EBS volume restore mount point",
   :description => "The EBS volume restore mount point",
-  :default => nil
+  :default => "/mnt/ebs"
 
 attribute "ebs/restore_backup_prefix",
   :display_name => "EBS volume restore backup prefix",
   :description => "The EBS volume restore backup prefix",
-  :default => nil
+  :default => "mybackup"
   
 attribute "ebs/restore_prefix_override",
   :display_name => "EBS volume restore prefix override",
   :description => "Override the EBS volume restore backup prefix",
-  :default => nil
+  :default => ""
