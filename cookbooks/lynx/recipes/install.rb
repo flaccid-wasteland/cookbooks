383aca55e97f78b841618583482d9003febf4bed
Chris Fordham <chris@xhost.com.au>
2010-10-05 01:25:30 +1100

    Add default/install recipes

--- cookbooks/lynx/recipes/install.rb
+++ cookbooks/lynx/recipes/install.rb
@@ -16,4 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-include_recipe "lynx::install"
+
+package "lynx" do
+  action :install
+end
