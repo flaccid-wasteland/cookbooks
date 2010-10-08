59bf27d1b7988df58e1c7b1b2f638402a02b99e7
Chris Fordham <chris@xhost.com.au>
2010-10-08 14:05:38 +1100

    Updated metadata

--- cookbooks/rs_ebs/metadata.rb
+++ cookbooks/rs_ebs/metadata.rb
@@ -33,3 +33,28 @@ attribute "ebs/restore_prefix_override",
   :display_name => "EBS volume restore prefix override",
   :description => "Override the EBS volume restore backup prefix",
   :default => ""
+
+attribute "ebs/backup_keep_last",
+  :display_name => "EBS backup keep last",
+  :description => "The total number of snapshots to keep.  The oldest snapshot will be deleted when this value is exceeded.  Default: 60.  See \"Archiving Snapshots\" on RightScale Support for further details on the archiving logic.",
+  :default => "60"
+
+attribute "ebs/backup_keep_daily",
+  :display_name => "EBS backup keep daily",
+  :description => "The number of daily snapshots to keep (i.e. rotation size). Default: 14.  See \"Archiving Snapshots\" on RightScale Support for further details on the archiving logic.",
+  :default => "14"    
+
+attribute "ebs/backup_keep_monthly",
+  :display_name => "EBS backup keep monthly",
+  :description => "The number of monthly snapshots to keep (i.e. rotation size).  Default: 12.  See \"Archiving Snapshots\" on RightScale Support for further details on the archiving logic.",
+  :default => "12"
+
+attribute "ebs/backup_keep_weekly",
+  :display_name => "EBS backup keep weekly",
+  :description => "The number of weekly snapshots to keep (i.e. rotation size). Default: 6.  See \"Archiving Snapshots\" on RightScale Support for further details on the archiving logic.",
+  :default => "4"
+
+attribute "ebs/backup_keep_yearly",
+  :display_name => "EBS backup keep yearly",
+  :description => "The number of yearly snapshots to keep (i.e. rotation size). Default: 2.  See \"Archiving Snapshots\" on RightScale Support for further details on the archiving logic.",
+  :default => "2"
