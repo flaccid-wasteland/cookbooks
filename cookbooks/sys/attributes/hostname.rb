f014bb3a64c9df20962f2f94b02bb831ec9eb066
Chris Fordham <chris@xhost.com.au>
2010-10-11 20:27:37 +1100

    Removed

--- cookbooks/sys/attributes/hostname.rb
+++
@@ -1,5 +0,0 @@
-default[:sys][:hostname] = "localhost"
-default[:sys][:fqdn] = ''
-default[:sys][:hostname_short] = nil
-default[:sys][:domain_name] = "localdomain"
-default[:sys][:domain_suffix_search] = nil
