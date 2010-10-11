95ec868f890794f85c91f907cd994b109c610b4c
Chris Fordham <chris@xhost.com.au>
2010-10-11 18:55:16 +1100

    Test attr

---
+++ cookbooks/sys/attributes/default.rb
@@ -0,0 +1,5 @@
+default[:sys][:hostname] = "localhost"
+default[:sys][:fqdn] = nil
+default[:sys][:shortname] = nil
+default[:sys][:domain] = "localdomain"
+default[:sys][:domain_suffix_search] = nil
