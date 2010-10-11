cf4a5110b4744350ea54b10242e250a5b8b02268
Chris Fordham <chris@xhost.com.au>
2010-10-11 17:41:24 +1100

    Add included recipes

--- cookbooks/postgresql/recipes/default.rb
+++ cookbooks/postgresql/recipes/default.rb
@@ -16,3 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+include_recipe "postgresql::client"
+include_recipe "postgresql::server"
