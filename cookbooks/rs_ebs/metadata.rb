e494d67ac37673108743ac8e8627a37dfc5e0ce5
Chris Fordham <chris@xhost.com.au>
2010-10-16 17:22:03 +1100

    Fix metadata

--- cookbooks/rs_ebs/metadata.rb
+++ cookbooks/rs_ebs/metadata.rb
@@ -18,53 +18,53 @@ recipe "rs_ebs::volume_backup", "Takes a regular EBS snapshot of an attached EBS
 attribute "ebs/restore_fs_type",
   :display_name => "EBS volume restore filesystem type",
   :description => "The EBS volume filestytem type",
-  :default => "xfs"
+  :default => "xfs",
   :recipes => [ "rs_ebs::volume_restore" ]
   
 attribute "ebs/mount_point",
   :display_name => "EBS volume restore mount point",
   :description => "The EBS volume restore mount point",
-  :default => "/mnt/ebs"
+  :default => "/mnt/ebs",
   :recipes => [ "rs_ebs::volume_restore" ]
 
 attribute "ebs/backup_prefix",
   :display_name => "EBS volume backup prefix",
   :description => "The EBS volume backup prefix",
-  :default => "mybackup"
+  :default => "mybackup",
   :recipes => [ "rs_ebs::volume_restore", "rs_ebs::volume_backup", "rs_ebs::volume_continuous_backups" ]
   
 attribute "ebs/restore_prefix_override",
   :display_name => "EBS volume restore prefix override",
   :description => "Override the EBS volume restore backup prefix",
-  :default => ""
+  :default => "",
   :recipes => [ "rs_ebs::volume_restore" ]
   
 attribute "ebs/backup_keep_last",
   :display_name => "EBS backup keep last",
   :description => "The total number of snapshots to keep.  The oldest snapshot will be deleted when this value is exceeded.  Default: 60.  See \"Archiving Snapshots\" on RightScale Support for further details on the archiving logic.",
-  :default => "60"
+  :default => "60",
   :recipes => [ "rs_ebs::volume_backup", "rs_ebs::volume_continuous_backups" ]
 
 attribute "ebs/backup_keep_daily",
   :display_name => "EBS backup keep daily",
   :description => "The number of daily snapshots to keep (i.e. rotation size). Default: 14.  See \"Archiving Snapshots\" on RightScale Support for further details on the archiving logic.",
-  :default => "14"    
+  :default => "14",
   :recipes => [ "rs_ebs::volume_backup", "rs_ebs::volume_continuous_backups" ]
 
 attribute "ebs/backup_keep_monthly",
   :display_name => "EBS backup keep monthly",
   :description => "The number of monthly snapshots to keep (i.e. rotation size).  Default: 12.  See \"Archiving Snapshots\" on RightScale Support for further details on the archiving logic.",
-  :default => "12"
+  :default => "12",
   :recipes => [ "rs_ebs::volume_backup", "rs_ebs::volume_continuous_backups" ]
 
 attribute "ebs/backup_keep_weekly",
   :display_name => "EBS backup keep weekly",
   :description => "The number of weekly snapshots to keep (i.e. rotation size). Default: 6.  See \"Archiving Snapshots\" on RightScale Support for further details on the archiving logic.",
-  :default => "4"
+  :default => "4",
   :recipes => [ "rs_ebs::volume_backup", "rs_ebs::volume_continuous_backups" ]
   
 attribute "ebs/backup_keep_yearly",
   :display_name => "EBS backup keep yearly",
   :description => "The number of yearly snapshots to keep (i.e. rotation size). Default: 2.  See \"Archiving Snapshots\" on RightScale Support for further details on the archiving logic.",
-  :default => "2"
+  :default => "2",
   :recipes => [ "rs_ebs::volume_backup", "rs_ebs::volume_continuous_backups" ]
