51e32eb017b4584c9cc91b975dade2de778e94f1
Chris Fordham <chris@xhost.com.au>
2010-10-17 13:07:00 +1100

    Update metadata to include recipes.

--- cookbooks/lynx/metadata.rb
+++ cookbooks/lynx/metadata.rb
@@ -1,6 +1,9 @@
 maintainer       "Chris Fordham"
 maintainer_email "chris@xhost.com.au"
 license          "Apache 2.0"
-description      "Installs/Configures lynx"
+description      "Installs/Configures the Lynx www text browser"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+
+recipe "lynx::default", "Installs Lynx via ::install"
+recipe "lynx::install", "Installs Lynx"
