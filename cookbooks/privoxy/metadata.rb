f7d9835a136575d20367a7e6d491ff16506bbb21
Chris Fordham <chris@xhost.com.au>
2010-10-19 17:34:04 +1100

    Fix recipe names in metadata

--- cookbooks/privoxy/metadata.rb
+++ cookbooks/privoxy/metadata.rb
@@ -5,5 +5,5 @@ description      "Installs/Configures privoxy"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 
-recipe "lynx::default", "Installs Privoxy via ::install"
-recipe "lynx::install", "Installs Privoxy"
+recipe "privoxy::default", "Installs Privoxy via ::install"
+recipe "privoxy::install", "Installs Privoxy"
