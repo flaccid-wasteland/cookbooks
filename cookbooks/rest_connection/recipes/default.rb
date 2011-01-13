43c5646ebac2e90c0f3129b7e368ef26fc595ca0
Xhost <admin@xhost.com.au>
2011-01-13 15:58:40 +1100

    Fix include recipe name.

--- cookbooks/rest_connection/recipes/default.rb
+++ cookbooks/rest_connection/recipes/default.rb
@@ -16,4 +16,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-include_recipe "rest_connection:install"
+include_recipe "rest_connection::install"
