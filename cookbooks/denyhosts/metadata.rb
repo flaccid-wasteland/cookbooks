716e8f8b8b23004b3fe4e1d3fc341679f8cba3d5
Xhost <admin@xhost.com.au>
2011-01-27 14:36:03 +1100

    Update metadata.

--- cookbooks/denyhosts/metadata.rb
+++ cookbooks/denyhosts/metadata.rb
@@ -1,6 +1,8 @@
 maintainer       "Chris Fordham"
 maintainer_email "chris@xhost.com.au"
 license          "Apache 2.0"
-description      "Installs/Configures denyhosts"
+description      "Installs/Configures DenyHosts"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+
+recipe "denyhosts::default", "Installs DenyHosts"
