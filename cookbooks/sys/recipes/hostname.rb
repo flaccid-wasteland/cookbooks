c0dd7bd545e050189924719c809023e67c45a07d
Xhost <admin@xhost.com.au>
2011-04-02 12:09:20 +1100

    Update logging method

--- cookbooks/sys/recipes/hostname.rb
+++ cookbooks/sys/recipes/hostname.rb
@@ -32,17 +32,17 @@ end
 
 hosts_ip = "#{local_ip}"
 
-ruby_block "show_hosts_info" do
+ruby_block "show_host_info" do
   block do
     def show_host_info
-      # Display current hostname values
-      Chef::Log.info("Host information:")
-      Chef::Log.info("* Hostname: #{`hostname` == '' ? '<none>' : `hostname`}")
-      Chef::Log.info("* Network node hostname: #{`uname -n` == '' ? '<none>' : `uname -n`}")
-      Chef::Log.info("* Alias names of host: #{`hostname -a` == '' ? '<none>' : `hostname -a`}")
-      Chef::Log.info("* Short host name (cut from first dot of hostname): #{`hostname -s` == '' ? '<none>' : `hostname -s`}")
-      Chef::Log.info("* Domain of hostname: #{`domainname` == '' ? '<none>' : `domainname`}")
-      Chef::Log.info("* FQDN of host: #{`hostname -f` == '' ? '<none>' : `hostname -f`}")
+      # Display current hostname values in log
+      log "Host/node information:"
+      log "* Hostname: #{`hostname` == '' ? '<none>' : `hostname`}"
+      log "* Network node hostname: #{`uname -n` == '' ? '<none>' : `uname -n`}"
+      log "* Alias names of host: #{`hostname -a` == '' ? '<none>' : `hostname -a`}"
+      log "* Short host name (cut from first dot of hostname): #{`hostname -s` == '' ? '<none>' : `hostname -s`}"
+      log "* Domain of hostname: #{`domainname` == '' ? '<none>' : `domainname`}"
+      log "* FQDN of host: #{`hostname -f` == '' ? '<none>' : `hostname -f`}"
     end
   end
   action :create
