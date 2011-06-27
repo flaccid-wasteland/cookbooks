f75627bbb36630f0edc3df919df41d7e4f55e6ac
Chris Fordham <chris@xhost.com.au>
2011-06-27 16:00:29 +1000

    Initial add of rubygems cookbook.

---
+++ cookbooks/rubygems/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Chris Fordham"
+maintainer_email "chris@xhost.com.au"
+license          "Apache 2.0"
+description      "Installs/Configures rubygems"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+
+recipe "rubygems::default", "Installs RubyGems"
