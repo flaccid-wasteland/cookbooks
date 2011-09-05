08e3b16c257a023586a0b658db74a5735fbcfcda
Chris Fordham <chris@xhost.com.au>
2011-09-05 17:31:07 +1000

    Use php-devel for centos.

--- cookbooks/pecl/attributes/default.rb
+++ cookbooks/pecl/attributes/default.rb
@@ -2,7 +2,7 @@ default['pecl']['packages'] =  nil
 
 case platform
 when "centos"
-  default['pecl']['dep_sys_packages'] =  %w{ php-pear php-dev }
+  default['pecl']['dep_sys_packages'] =  %w{ php-pear php-devel }
 when "debian","ubuntu"
   default['pecl']['dep_sys_packages'] =  %w{ php-pear php5-dev }
 end
