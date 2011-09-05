2378889dfa8e6107355bcbfa1092d7970d925c2b
Chris Fordham <chris@xhost.com.au>
2011-09-05 16:30:34 +1000

    Add pkg php-pear to default.

--- cookbooks/pecl/attributes/default.rb
+++ cookbooks/pecl/attributes/default.rb
@@ -2,7 +2,7 @@ default['pecl']['packages'] =  nil
 
 case platform
 when "centos"
-  default['pecl']['dep_sys_packages'] =  %w{ php-dev }
+  default['pecl']['dep_sys_packages'] =  %w{ php-pear php-dev }
 when "debian","ubuntu"
-  default['pecl']['dep_sys_packages'] =  %w{ php5-dev }
+  default['pecl']['dep_sys_packages'] =  %w{ php-pear php5-dev }
 end
