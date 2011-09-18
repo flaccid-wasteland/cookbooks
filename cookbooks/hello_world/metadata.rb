60704f6c7815dca789d54c8cc5d81c8faa3d81ed
Chris Fordham <chris@xhost.com.au>
2011-09-18 23:13:44 +1000

    Initial add of hello_world cookbook.

---
+++ cookbooks/hello_world/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Chris Fordham"
+maintainer_email "chris@xhost.com.au"
+license          "Apache 2.0"
+description      "Simple Hello World! demonstration cookbook."
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+
+recipe "hello_world::default", "Prints Hello World! to Chef log."
