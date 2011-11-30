b7deec52ccb4c2d56716638dc178f353badc1001
Chris Fordham <chris@xhost.com.au>
2011-11-30 16:27:21 +1100

    Add install_source attribute.

--- cookbooks/ruby/metadata.rb
+++ cookbooks/ruby/metadata.rb
@@ -6,3 +6,11 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 
 recipe "ruby::default", "Installs ruby"
+
+attribute "ruby/install_source",
+  :display_name => "Ruby Install Source",
+  :description => "The install source for Ruby: none or package (default: package).",
+  :default => "package",
+  :required => "required",
+  :choices => [ "package", "none" ],
+  :recipes => [ "ruby::default" ]
