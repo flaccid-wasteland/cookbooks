36bba089288acc7213baa693a7a38e8217e5c607
Chris Fordham <chris@xhost.com.au>
2011-11-30 19:53:27 +1100

    Add include of fog default recipe.

--- cookbooks/fog/recipes/test.rb
+++ cookbooks/fog/recipes/test.rb
@@ -14,6 +14,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 
+include_recipe "fog"
+
 script "test_fog" do
   interpreter "/usr/bin/ruby"
   code <<-EOH
