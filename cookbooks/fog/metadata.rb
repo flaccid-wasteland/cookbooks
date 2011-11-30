ebbc1194d1abb61cf66c58bebbf24b73f2a2f92a
Chris Fordham <chris@xhost.com.au>
2011-11-30 19:25:55 +1100

    Add attributes for AWS creds.

--- cookbooks/fog/metadata.rb
+++ cookbooks/fog/metadata.rb
@@ -8,3 +8,13 @@ version          "0.0.1"
 recipe "fog::default", "Installs and configures Fog"
 recipe "fog::install", "Installs Fog"
 recipe "fog::install_delayed", "Installs Fog delayed (for using the rvm cookbook on RightScale)."
+
+attribute "aws/access_key_id",
+  :display_name => "AWS Access Key ID",
+  :description => "The AWS Access Key ID.",
+  :recipes => [ "rvm::test" ]
+
+attribute "aws/secret_access_key",
+  :display_name => "AWS Secret Access Key",
+  :description => "The AWS Secret Access Key.",
+  :recipes => [ "rvm::test" ]
