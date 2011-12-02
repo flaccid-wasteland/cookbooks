33dc09c1a704714225cdd7934457425240674e53
Chris Fordham <chris@xhost.com.au>
2011-12-02 15:43:22 +1100

    Test package resource only.

--- cookbooks/rest_connection/recipes/install.rb
+++ cookbooks/rest_connection/recipes/install.rb
@@ -1,4 +1,3 @@
-#
 # Cookbook Name:: rest_connection
 # Recipe:: install
 #
@@ -15,14 +14,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-gem_package "rest_connection" do
-  action :install
-end
 
-gem_package "rest_connection" do
-  gem_binary "/opt/rightscale/sandbox/bin/gem"
+package "rest_connection" do
   action :install
+  provider Chef::Provider::Package::Rubygems
 end
-
-Gem.clear_paths
