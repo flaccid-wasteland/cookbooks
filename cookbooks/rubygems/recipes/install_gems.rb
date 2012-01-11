4c739d7741018db3b6eb7c8be22cc2ba321daa39
Chris Fordham <chris@xhost.com.au>
2012-01-11 18:11:50 +1100

    Add new recipes.

---
+++ cookbooks/rubygems/recipes/install_gems.rb
@@ -0,0 +1,34 @@
+# Cookbook Name:: rubygems
+# Recipe:: install_gems
+#
+# Copyright 2011, Chris Fordham
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "ruby::default"
+include_recipe "rubygems::default"
+
+# split the gems string into an array when an array is not provided
+if node[:rubygems][:gems_install].kind_of?(Array)
+	gems = node[:rubygems][:gems_install]
+else
+	gems = node[:rubygems][:gems_install].split(/ /)
+end
+
+gems.each do |g|
+  gem_package g do
+    gem_binary "/usr/bin/gem"
+    only_if { File.exists?('/opt/rightscale/sandbox/bin') }
+  end
+end
