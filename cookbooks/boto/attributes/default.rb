7cc5294fd6f6fabe505a65d62035b0cb2c2cef10
Chris Fordham <chris@xhost.com.au>
2012-01-30 15:57:01 +1100

    Add attributes for boto.cfg template.

--- cookbooks/boto/attributes/default.rb
+++ cookbooks/boto/attributes/default.rb
@@ -4,6 +4,12 @@ default['boto']['package_prefix'] = "boto-"
 default['boto']['src_version'] = "2.1.1"
 default['boto']['src_checksum'] = "5528f3010c42dd0ed7b188a6917295f1"
 default['boto']['install_method'] = 'package'
+default['boto']['num_retries'] = 10
+default['boto']['debug'] = 0
+
+# AWS credentials
+default['boto']['aws_access_key_id'] = nil
+default['boto']['aws_secret_access_key'] = nil
 
 default['boto']['pkg_filename'] = "#{node['boto']['package_prefix']}#{node['boto']['src_version']}#{node['boto']['package_extension']}"
 default['boto']['pkg_url'] = "#{node['boto']['mirror_url_prefix']}#{node['boto']['pkg_filename']}"
