4292dac57c282753780e0e70a2b568acd96655cf
Chris Fordham <chris@xhost.com.au>
2012-01-30 18:28:55 +1100

    Add include python recipe.

--- cookbooks/boto/recipes/install.rb
+++ cookbooks/boto/recipes/install.rb
@@ -15,4 +15,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+include_recipe "python"
 include_recipe "boto::install_from_#{node['boto']['install_method']}"
