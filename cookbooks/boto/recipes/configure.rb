904467772b49501728cca837ac91c128b98ba651
Chris Fordham <chris@xhost.com.au>
2012-01-30 15:57:13 +1100

    Add template resource for /etc/boto.cfg.

--- cookbooks/boto/recipes/configure.rb
+++ cookbooks/boto/recipes/configure.rb
@@ -14,3 +14,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+template "/etc/boto.cfg" do
+  source "boto.cfg.erb"
+end
