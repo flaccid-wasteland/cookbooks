a4b83749275077ba54ce7bc0929c15b962fdb7c0
Chris Fordham <chris@xhost.com.au>
2012-01-30 17:37:35 +1100

    Add required options to attributes.

--- cookbooks/boto/metadata.rb
+++ cookbooks/boto/metadata.rb
@@ -17,48 +17,57 @@ attribute "boto/install_method",
   :display_name => "boto Install Method",
   :description => "The method used to install the boto library.",
   :default => "package",
+  :required => "recommended",
   :choice => [ 'package', 'pip', 'source' ],
   :recipes => [ "boto::install" ]
 
  attribute "boto/aws_access_key_id",
   :display_name => "boto AWS Access Key ID",
   :description => "AWS Access Key ID for boto.",
+  :required => "required",
   :recipes => [ "boto::configure" ]
 
  attribute "boto/aws_secret_access_key",
   :display_name => "boto AWS Secret Access Key",
   :description => "AWS Secrete Access Key for boto.",
+  :required => "required",
   :recipes => [ "boto::configure" ]
 
  attribute "boto/num_retries",
   :display_name => "boto Number Retries",
   :description => "The number of times boto retries an action.",
   :default => '10',
+  :required => "optional",
   :recipes => [ "boto::configure" ]
 
 attribute "boto/debug",
   :display_name => "boto Debug Level",
   :description => "The debug level for boto.",
   :default => '0',
+  :required => "optional",
   :recipes => [ "boto::configure" ]
 
 attribute "boto/debug",
   :display_name => "boto Debug Level",
   :description => "The debug level for boto.",
   :default => '0',
+  :required => "optional",
   :recipes => [ "boto::configure" ]
 
 attribute "boto/s3_fetch_bucket",
   :display_name => "boto S3 Fetch Bucket",
   :description => "The bucket to use when fetching a file with s3_fetch_file.",
+  :required => "optional",
   :recipes => [ "boto::s3_fetch_file" ]
 
 attribute "boto/s3_fetch_file",
   :display_name => "boto S3 Fetch File",
   :description => "The S3 key/file to fetch with s3_fetch_file.",
+  :required => "optional",
   :recipes => [ "boto::s3_fetch_file" ]
 
 attribute "boto/s3_fetch_file_destination",
   :display_name => "boto S3 Fetch File Dest",
   :description => "The destination file/folder to store the fetched file with s3_fetch_file",
+  :required => "optional",
   :recipes => [ "boto::s3_fetch_file" ]
