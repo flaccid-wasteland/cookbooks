e703a2555ec693f57f2654748ad169677589cc68
Chris Fordham <chris@xhost.com.au>
2012-01-30 12:28:05 +1100

    Add install_method attribute.

--- cookbooks/boto/attributes/default.rb
+++ cookbooks/boto/attributes/default.rb
@@ -3,6 +3,7 @@ default['boto']['package_extension'] = ".tar.gz"
 default['boto']['package_prefix'] = "boto-"
 default['boto']['src_version'] = "2.1.1"
 default['boto']['src_checksum'] = "5528f3010c42dd0ed7b188a6917295f1"
+default['boto']['install_method'] = 'pip'
 
 default['boto']['pkg_filename'] = "#{node['boto']['package_prefix']}#{node['boto']['src_version']}#{node['boto']['package_extension']}"
 default['boto']['pkg_url'] = "#{node['boto']['mirror_url_prefix']}#{node['boto']['pkg_filename']}"
