4465250a59e6beebaccb7cd1c8bcf116ce2e0bcf
Chris Fordham <chris@xhost.com.au>
2012-01-30 15:41:00 +1100

    Add more recipes and attributes to metadata.

--- cookbooks/boto/metadata.rb
+++ cookbooks/boto/metadata.rb
@@ -7,3 +7,13 @@ version          "0.0.1"
 
 recipe "boto::default", "Installs & configures boto."
 recipe "boto::install", "Installs boto."
+recipe "boto::configure", "Configures boto."
+recipe "boto::install_from_package", "Installs boto by package."
+recipe "boto::install_from_pip", "Installs boto using PIP."
+recipe "boto::install_from_source", "Installs boto from source."
+
+attribute "boto/install_method",
+  :display_name => "boto Install Method",
+  :description => "The method used to install the boto library.",
+  :default => "package",
+  :recipes => [ "boto::install" ]
