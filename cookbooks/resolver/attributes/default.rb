a67fbc4e47bfe5f02769107f095b59a52c1cb9f7
Chris Fordham <chris@xhost.com.au>
2012-02-24 21:58:37 +1100

    Use key syntax.

--- cookbooks/resolver/attributes/default.rb
+++ cookbooks/resolver/attributes/default.rb
@@ -1,2 +1,2 @@
-default[:resolver][:search] = `domainname`    # assumes valid domain name from fqdn chef requirement
-default[:resolver][:nameservers] = nil
+default['resolver']['search'] = `domainname`    # assumes valid domain name from fqdn chef requirement
+default['resolver']['nameservers'] = nil
