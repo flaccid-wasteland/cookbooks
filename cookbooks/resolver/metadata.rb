0bcda248d4dc3c006f1ee1bf949df78d84388c6b
Chris Fordham <chris@xhost.com.au>
2012-02-25 03:17:58 +1100

    Update metadata.

--- cookbooks/resolver/metadata.rb
+++ cookbooks/resolver/metadata.rb
@@ -6,7 +6,7 @@ long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.8.2"
 
 recipe "resolver", "Configures /etc/resolv.conf via attributes."
-recipe "resolver::rs_run_resolver", "runs resolver recipe with rs_run_recipe."
+recipe "resolver::rs_run_resolver", "Runs the rs_run_resolver recipe with rs_run_recipe (for RightScale servers only)."
 
 %w{ ubuntu debian fedora centos redhat freebsd openbsd macosx }.each do |os|
   supports os
