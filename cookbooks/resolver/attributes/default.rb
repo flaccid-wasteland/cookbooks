151415cc058d4331158d2908ad8f1fcda2417341
Chris Fordham <chris@xhost.com.au>
2012-02-24 22:54:08 +1100

    Change nameservers default to nil.

--- cookbooks/resolver/attributes/default.rb
+++ cookbooks/resolver/attributes/default.rb
@@ -1,2 +1,3 @@
 default['resolver']['search'] = `domainname`    # assumes valid domain name from fqdn chef requirement
 default['resolver']['nameservers'] = nil
+#default['resolver']['nameservers'] = [ '8.8.8.8', '4.2.2.1' ]
