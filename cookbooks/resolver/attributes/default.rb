0fa75ce7b1dc5a30f06546dbec75e3671be0b5a7
Chris Fordham <chris@xhost.com.au>
2012-02-24 20:14:00 +1100

    Move use of dnsruby into recipe instead of attributes.

--- cookbooks/resolver/attributes/default.rb
+++ cookbooks/resolver/attributes/default.rb
@@ -1,4 +1,2 @@
-require 'dnsruby'
-
 default[:resolver][:search] = `domainname`    # assumes valid domain name from fqdn chef requirement
-default[:resolver][:nameservers] = Dnsruby::Config::new::nameserver()   # assumes current nameservers
+default[:resolver][:nameservers] = nil
