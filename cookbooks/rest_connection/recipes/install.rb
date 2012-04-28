628761ec707b3c1415ade51a6a195aa5a7386db6
Chris Fordham <chris@xhost.com.au>
2012-04-28 13:51:08 +1000

    Add install of dep pkgs and logic for RightScale system install.

--- cookbooks/rest_connection/recipes/install.rb
+++ cookbooks/rest_connection/recipes/install.rb
@@ -1,8 +1,7 @@
-#
 # Cookbook Name:: rest_connection
 # Recipe:: install
 #
-# Copyright 2011, Chris Fordham
+# Copyright 2011-2012, Chris Fordham
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,14 +14,25 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-gem_package "rest_connection" do
-  action :install
-end
 
-gem_package "rest_connection" do
-  gem_binary "/opt/rightscale/sandbox/bin/gem"
-  action :install
+# install rest_connection build/install deps
+pkg_deps = value_for_platform([ "centos", "redhat", "suse", "fedora" ] => {"default" => [ "libxml2", "libxml2-devel libxslt-devel"]}, [ "ubuntu", "debian" ] => {"default" => [ "libxml2", "libxml2-dev", "libxslt-dev"]}, "archlinux" => {"default" => [ "libxml2", "libxslt"]})
+
+pkg_deps.each { |pkg|
+  package pkg
+} unless ! pkg_deps
+
+gem_package "rest_connection"
+gem_package "i18n"
+
+# install to system as well if on RightScale
+if defined?(RightScale)
+  gem_package "rest_connection" do
+    gem_binary "/usr/bin/gem"
+  end
+  gem_package "i18n" do
+    gem_binary "/usr/bin/gem"
+  end
 end
 
 Gem.clear_paths
