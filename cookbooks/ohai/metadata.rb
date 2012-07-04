8fd2c99d94dabd003789ddf69377e20a1c88bba7
Chris Fordham <chris.fordham@rightscale.com>
2012-07-04 13:03:11 +1000

    Add new recipes to metadata.

--- cookbooks/ohai/metadata.rb
+++ cookbooks/ohai/metadata.rb
@@ -5,4 +5,6 @@ description      "Installs/Configures ohai"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
+recipe "ohai::default", "Includes the ohai::print_node_attributes recipe only."
 recipe "ohai::reload_all_plugins", "Reloads all ohai plugins in a Chef run."
+recipe "ohai::print_node_attributes", "Prints the node[] object using a log resource."
