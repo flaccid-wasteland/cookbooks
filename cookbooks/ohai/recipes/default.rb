43fe1afab0aa70ffd68f846f7db833fdfe3525c7
Chris Fordham <chris.fordham@rightscale.com>
2012-07-04 12:50:50 +1000

    Add in include of print recipe.

--- cookbooks/ohai/recipes/default.rb
+++ cookbooks/ohai/recipes/default.rb
@@ -1,4 +1,3 @@
-#
 # Cookbook Name:: ohai
 # Recipe:: default
 #
@@ -15,4 +14,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+
+# by default prints the node object
+include_recipe "ohai::print_node_attributes"
