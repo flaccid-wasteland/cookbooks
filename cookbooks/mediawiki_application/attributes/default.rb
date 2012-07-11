4a332e2682f30e536ce7ed443817a2e374285fb9
Chris Fordham <chris.fordham@rightscale.com>
2012-07-11 17:33:44 +1000

    Add attribute defaults for mediawiki_application cookbook.

---
+++ cookbooks/mediawiki_application/attributes/default.rb
@@ -0,0 +1,6 @@
+default['mediawiki_application']['name'] = 'mediawiki'
+default['mediawiki_application']['path'] = '/var/www'
+default['mediawiki_application']['system_user'] = 'root'
+default['mediawiki_application']['system_group'] = 'root'
+default['mediawiki_application']['repository_url'] = "https://gerrit.wikimedia.org/r/p/mediawiki/core.git"
+default['mediawiki_application']['revision'] = 'master'
