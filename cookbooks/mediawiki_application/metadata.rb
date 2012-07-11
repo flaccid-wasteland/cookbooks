db8ab2193e28dcac159f452d8555e8a0cbc7230e
Chris Fordham <chris.fordham@rightscale.com>
2012-07-11 15:37:46 +1000

    Initial add of mediawiki_application cookbook.

---
+++ cookbooks/mediawiki_application/metadata.rb
@@ -0,0 +1,60 @@
+maintainer       "Chris Fordham"
+maintainer_email "chris@xhost.com.au"
+license          "Apache 2.0"
+description      "Installs/Configures mediawiki_application"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+depends          "python"
+depends          "runit"
+depends          "unicorn"
+depends          "passenger_apache2"
+depends          "tomcat"
+depends          "gunicorn"
+depends          "apache2"
+depends          "php"
+
+recipe "mediawiki_application::default", "Installs & configures Mediawiki."
+recipe "mediawiki_application::applications", "Installs & configures Mediawiki."
+
+attribute "mediawiki_application/name",
+  :display_name => "MediaWiki Application Name",
+  :description => "The name of the MediaWiki application.",
+  :required => "required",
+  :recipes => [ "mediawiki_application::application" ]
+
+attribute "mediawiki_application/path",
+  :display_name => "MediaWiki Install Path",
+  :description => "The path to install MediaWiki to, e.g. /var/www",
+  :default => "/usr/local/www",
+  :required => "required",
+  :choice => [ '/usr/local/www', '/var/www' ],
+  :recipes => [ "mediawiki_application::application" ]
+    
+attribute "mediawiki_application/system_user",
+  :display_name => "MediaWiki System User",
+  :description => "The system user (owner) for the application.",
+  :default => "www-data",
+  :required => "required",
+  :choice => [ 'www-data', 'httpd', 'http', 'www', 'root' ],
+  :recipes => [ "mediawiki_application::application" ]
+      
+attribute "mediawiki_application/system_group",
+  :display_name => "MediaWiki System Group",
+  :description => "The system group for the application.",
+  :default => "www-data",
+  :required => "required",
+  :choice => [ 'www-data', 'httpd', 'http', 'www', 'root', 'users' ],
+  :recipes => [ "mediawiki_application::application" ]
+  
+attribute "mediawiki_application/repository_url",
+  :display_name => "MediaWiki Repository URL",
+  :description => "The URL to the application for checkout/download.",
+  :required => "required",
+  :recipes => [ "mediawiki_application::application" ]
+    
+attribute "mediawiki_application/revision",
+  :display_name => "MediaWiki Application Revision",
+  :description => "The revision of the application to use (tag/branch/commit).",
+  :required => "required",
+  :recipes => [ "mediawiki_application::application" ]
---
+++ cookbooks/mediawiki_application/recipes/application.rb
@@ -0,0 +1,29 @@
+# Cookbook Name:: mediawiki_application
+# Recipe:: application
+#
+# Copyright 2012, Chris Fordham
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+application node['mediawiki_application']['name'] do
+  path node['mediawiki_application']['path']
+  owner node['mediawiki_application']['system_user']
+  group node['mediawiki_application']['system_group']
+
+  repository node['mediawiki_application']['repository_url']
+  revision node['mediawiki_application']['revision']
+
+  php do
+    # php-specific configuration
+  end
+end
