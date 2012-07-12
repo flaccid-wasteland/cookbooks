9628465f9cd84ca5f722134b76149a02e6208747
Chris Fordham <chris.fordham@rightscale.com>
2012-07-12 11:22:26 +1000

    Add include of git and subversion recipes.

--- cookbooks/mediawiki_application/recipes/application.rb
+++ cookbooks/mediawiki_application/recipes/application.rb
@@ -15,6 +15,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+include_recipe "git"
+include_recipe "subversion"
+
 application node['mediawiki_application']['name'] do
   path node['mediawiki_application']['path']
   owner node['mediawiki_application']['system_user']
