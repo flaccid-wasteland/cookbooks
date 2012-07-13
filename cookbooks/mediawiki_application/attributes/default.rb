285fc42fc134eedbfec7aa487c5724dda1f7ae34
Chris Fordham <chris.fordham@rightscale.com>
2012-07-13 13:07:18 +1000

    Change spelling to 'adapter'.

--- cookbooks/mediawiki_application/attributes/default.rb
+++ cookbooks/mediawiki_application/attributes/default.rb
@@ -5,7 +5,7 @@ default['mediawiki_application']['system_group'] = 'root'
 default['mediawiki_application']['repository_url'] = "https://gerrit.wikimedia.org/r/p/mediawiki/core.git"
 default['mediawiki_application']['revision'] = 'master'
 
-default['mediawiki_application']['database']['adaptor'] = 'mysql'
+default['mediawiki_application']['database']['adapter'] = 'mysql'
 default['mediawiki_application']['database']['name'] = 'mediawiki'
 default['mediawiki_application']['database']['username'] = 'mediawiki'
 default['mediawiki_application']['database']['password'] = nil
