7f0e7a7bcc968ae103fb4ef6739e5234db26c804
Chris Fordham <chris.fordham@rightscale.com>
2012-07-20 19:38:23 +1000

    Add space between resources.

--- cookbooks/mediawiki_application/recipes/drop_database_user.rb
+++ cookbooks/mediawiki_application/recipes/drop_database_user.rb
@@ -15,6 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+log "Dropping database user, #{node['mediawiki_application']['db']['username']}"
+
 mysql_database_user node['mediawiki_application']['db']['username'] do
   connection ( {:host => node['mediawiki_application']['db']['host'], :username => 'root', :password => node['mysql']['server_root_password']} )
   action :drop
