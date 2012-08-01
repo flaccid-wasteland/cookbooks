00aee69b8c17347652244b3f6568f33c6a3f9c0a
Chris Fordham <chris.fordham@rightscale.com>
2012-08-01 21:59:02 +1000

    Add fqdn and domain tags.

--- cookbooks/rightscale/recipes/server_tags.rb
+++ cookbooks/rightscale/recipes/server_tags.rb
@@ -15,6 +15,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+rightscale_tag "server:fqdn=#{`hostname --fqdn`.strip}"
+rightscale_tag "server:domain=#{`hostname --domain`.strip}"
+
 rightscale_tag "server:uuid=#{node['rightscale']['instance_uuid']}" do
   only_if { node['rightscale']['instance_uuid'] }
 end
