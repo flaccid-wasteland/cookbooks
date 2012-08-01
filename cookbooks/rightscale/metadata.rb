7a0db352d04a579468a365900c5a6e38ff057d17
Chris Fordham <chris.fordham@rightscale.com>
2012-08-01 21:45:54 +1000

    Add recipes to metadata.

--- cookbooks/rightscale/metadata.rb
+++ cookbooks/rightscale/metadata.rb
@@ -1,6 +1,11 @@
 maintainer       "Chris Fordham"
 maintainer_email "chris@xhost.com.au"
 license          "Apache 2.0"
-description      "Installs/Configures rightscale"
+description      "Installs/Configures a Rightscale Server node."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+recipe "rightscale::default", "Sets up server tags."
+recipe "rightscale::tools", "Installs RightScale tools."
+recipe "rightscale::monitoring", "Sets up and configures RightScale Monitoring."
+recipe "rightscale::server_tags", "Sets RightScale Server tags."
