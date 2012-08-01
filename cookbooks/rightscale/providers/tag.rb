45a162f5466c5e2e893fd92a1c5690edf2f3e031
Chris Fordham <chris.fordham@rightscale.com>
2012-08-01 22:09:05 +1000

    Add only_if which rs_tag to execute resource.

--- cookbooks/rightscale/providers/tag.rb
+++ cookbooks/rightscale/providers/tag.rb
@@ -2,6 +2,7 @@ action :add do
 
 execute "rs_tag #{new_resource.name}" do
   command "rs_tag --add '#{new_resource.name}'"
+  only_if "which rs_tag"
 end
 
 new_resource.updated_by_last_action(true)
