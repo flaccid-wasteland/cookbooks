74b1d19ff1524aae835f3588df655c502eaec2f0
Chris Fordham <chris.fordham@rightscale.com>
2012-08-01 22:35:16 +1000

    Add depends on collectd to metadata.

--- cookbooks/rightscale/metadata.rb
+++ cookbooks/rightscale/metadata.rb
@@ -5,6 +5,8 @@ description      "Installs/Configures a Rightscale Server node."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
+depends "collectd"
+
 recipe "rightscale::default", "Sets up server tags."
 recipe "rightscale::tools", "Installs RightScale tools."
 recipe "rightscale::monitoring", "Sets up and configures RightScale Monitoring."
