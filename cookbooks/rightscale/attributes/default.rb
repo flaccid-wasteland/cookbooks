e00acf4ebaf0b3a3f54e235cabf3eaaddfe8f207
Chris Fordham <chris.fordham@rightscale.com>
2012-08-02 14:22:50 +1000

    Add default attributes for collectd_plugins and sketchy/hostname.

--- cookbooks/rightscale/attributes/default.rb
+++ cookbooks/rightscale/attributes/default.rb
@@ -1,1 +1,5 @@
+default['rightscale']['monitoring']['collectd_plugins'] = [ "cpu", "df", "disk", "load", "memory", "processes", "swap", "users" ]
+
+# these are injected into the right_link run; defaults set for non rightscale nodes
 default['rightscale']['instance_uuid'] = false
+default['rightscale']['servers']['sketchy']['hostname'] = nil
