d70cee01cef8d08d894e0996fc015a66996b399a
Chris Fordham <chris.fordham@rightscale.com>
2012-08-03 13:59:04 +1000

    Add initial default attributes.

---
+++ cookbooks/phpmyadmin_application/attributes/default.rb
@@ -0,0 +1,17 @@
+default['phpmyadmin_application']['path'] = '/var/www'
+default['phpmyadmin_application']['system_user'] = 'root'
+default['phpmyadmin_application']['system_group'] = 'root'
+default['phpmyadmin_application']['repository_url'] = "git://github.com/phpmyadmin/phpmyadmin.git"
+default['phpmyadmin_application']['revision'] = 'master'
+default['phpmyadmin_application']['deploy_action'] = 'deploy'
+default['phpmyadmin_application']['webapp_template'] = "phpmyadmin_apache_vhost.conf.erb"
+
+default['phpmyadmin_application']['db']['host'] = 'localhost'      # localhost for a LAMP AIO
+default['phpmyadmin_application']['db']['adapter'] = 'mysql'
+default['phpmyadmin_application']['db']['schema'] = 'phpmyadmin'
+default['phpmyadmin_application']['db']['username'] = 'phpmyadmin'
+default['phpmyadmin_application']['db']['password'] = nil
+
+default['phpmyadmin_application']['php']['pear_packages'] = [ "mysql" ]
+default['phpmyadmin_application']['php']['local_settings_file'] = 'config.inc.php'
+default['phpmyadmin_application']['php']['settings_template'] = 'config.sample.inc.php.erb'
