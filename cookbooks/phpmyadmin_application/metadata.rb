206a224e14701de23bf9786678fa6a493b4e2c5e
Chris Fordham <chris.fordham@rightscale.com>
2012-08-03 14:02:31 +1000

    Add metadata.rb.

--- cookbooks/phpmyadmin_application/metadata.rb
+++ cookbooks/phpmyadmin_application/metadata.rb
@@ -4,3 +4,12 @@ license          "Apache 2.0"
 description      "Installs/Configures phpmyadmin_application"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+depends "git"
+depends "subversion"
+depends "runit"
+depends "passenger_apache2"
+depends "apache2"
+depends "php"
+depends "application"
+depends "application_php"
