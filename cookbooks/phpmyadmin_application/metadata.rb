a5c2c3a3d9167ce064b9b2bbbc1ed7f90484c1c0
Chris Fordham <chris.fordham@rightscale.com>
2012-08-03 15:10:57 +1000

    Add recipes and attributes to metadata and generate metadata.json.

--- cookbooks/phpmyadmin_application/metadata.rb
+++ cookbooks/phpmyadmin_application/metadata.rb
@@ -13,3 +13,15 @@ depends "apache2"
 depends "php"
 depends "application"
 depends "application_php"
+
+recipe "phpmyadmin_application::default", "Installs & configures phpmyadmin."
+recipe "phpmyadmin_application::application", "Installs & configures the phpmyadmin application including HTTP virtual host."
+recipe "phpmyadmin_application::php_mysql", "Installs & configures php-mysql support."
+
+attribute "phpmyadmin_application/db/host",
+  :display_name => "phpmyadmin Database Host",
+  :description => "The hostname of the MySQL database server to connect to.",
+  :required => "recommended",
+  :option => [ 'localhost' ],
+  :default => "localhost",
+  :recipes => [ "phpmyadmin_application::application", "phpmyadmin_application::default" ]
