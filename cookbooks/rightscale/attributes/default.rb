540ac9da676abcd46a6cea5e5b5680c7b8b72100
Chris Fordham <chris@xhost.com.au>
2012-09-04 12:02:19 +1000

    Add dev version of default.rb attributes.

---
+++ cookbooks/rightscale/attributes/default.rb
@@ -0,0 +1,7 @@
+default['rightscale']['package']['install_method'] = 'auto'
+
+default['rightscale']['package']['url']['ubuntu'] = "http://mirror.rightscale.com/rightlink/5.7.16/ubuntu/rightscale_5.7.16-ubuntu_10.04-amd64.deb"
+default['rightscale']['package']['url']['debian'] = "http://mirror.rightscale.com/rightlink/5.7.16/ubuntu/rightscale_5.7.16-ubuntu_10.04-amd64.deb"
+default['rightscale']['package']['url']['redhat'] = "http://mirror.rightscale.com/rightlink/5.7.17/redhatenterpriseserver/rightscale_5.7.17-rhel_5.6-x86_64.rpm"
+default['rightscale']['package']['url']['centos'] = "http://mirror.rightscale.com/rightlink/5.7.14/centos/rightscale_5.7.14-centos_5.6-x86_64.rpm"
+default['rightscale']['package']['url']['windows'] = "http://mirror.rightscale.com/rightlink/5.7.20/windows/Rightscale_Windows_x64_5.7.20.msi"
