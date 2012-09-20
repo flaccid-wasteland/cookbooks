0f5c4d6596542fa13d659837cf95eca7f443f408
Chris Fordham <chris.fordham@rightscale.com>
2012-09-20 16:23:49 +1000

    Add remove action to rightscale_tag LWRP.

--- cookbooks/rightscale/providers/tag.rb
+++ cookbooks/rightscale/providers/tag.rb
@@ -8,3 +8,14 @@ end
 new_resource.updated_by_last_action(true)
 
 end # close action :add
+
+action :remove do
+
+execute "rs_tag #{new_resource.name}" do
+  command "rs_tag --remove '#{new_resource.name}'"
+  only_if "which rs_tag"
+end
+
+new_resource.updated_by_last_action(true)
+
+end # close action :remove
--- cookbooks/rightscale/resources/tag.rb
+++ cookbooks/rightscale/resources/tag.rb
@@ -22,7 +22,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-actions :add
+actions :add, :remove
 
 def initialize(*args)
   super
