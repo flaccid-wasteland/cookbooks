69acb21fe6205d1324da0efaf9003a6c2fdc7599
Chris Fordham <chris.fordham@rightscale.com>
2012-10-15 12:24:20 +1100

    Minor spacing change to default.rb recipe.

--- cookbooks/mib/recipes/default.rb
+++ cookbooks/mib/recipes/default.rb
@@ -1,4 +1,3 @@
-
 # Cookbook Name:: mib
 # Recipe:: default
 #
@@ -15,4 +14,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
