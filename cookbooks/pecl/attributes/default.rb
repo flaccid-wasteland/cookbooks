fa38a590aad33cc593fa33558c27d556196f13ae
Chris Fordham <chris.fordham@rightscale.com>
2012-12-10 20:34:07 +1100

    Remove deprecated attributes.

--- cookbooks/pecl/attributes/default.rb
+++ cookbooks/pecl/attributes/default.rb
@@ -1,9 +1,2 @@
 default['pecl']['packages'] =  nil
 default['pecl']['packages_remove'] =  nil
-
-case platform
-when "centos"
-  default['pecl']['dep_sys_packages'] =  %w{ php-pear php-devel }
-when "debian","ubuntu"
-  default['pecl']['dep_sys_packages'] =  %w{ php-pear php5-dev }
-end
