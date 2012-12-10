7488f12febe4f08dc1ab315d56abec799c4a04e5
Chris Fordham <chris.fordham@rightscale.com>
2012-12-10 20:35:03 +1100

    Change metadata to depend on php cookbook and use arrays for packages attributes.

--- cookbooks/pecl/metadata.rb
+++ cookbooks/pecl/metadata.rb
@@ -5,18 +5,22 @@ description      "Installs/Configures pecl"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 
+depends "php"
+
 recipe "pecl::default", "Ensures PECL is installed so packages can be installed."
 recipe "pecl::install_packages", "Installs PECL packages."
 recipe "pecl::uninstall_packages", "Uninstalls PECL packages."
 
 attribute "pecl/packages",
   :display_name => "PECL packages",
-  :description => "The PECL packages to install, e.g. mongo apc",
+  :description => "The PECL packages to install, e.g. w{mongo apc}",
   :default => nil,
+  :type => "array",
   :recipes => [ "pecl::install_packages" ]
   
 attribute "pecl/packages_remove",
   :display_name => "PECL remove packages",
-  :description => "The PECL packages to uninstall, e.g. Xdebug memcache",
+  :description => "The PECL packages to uninstall, e.g. w{Xdebug memcache}",
   :default => nil,
+  :type => "array",
   :recipes => [ "pecl::uninstall_packages" ]
