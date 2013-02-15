ea868ac6304f6a3238a72c5b6e3ba67d0b40d762
Chris Fordham <chris.fordham@rightscale.com>
2013-02-15 13:05:26 +1100

    Add initial hack/feature to override node attributes via a json provided in user-data.

--- cookbooks/rightscale/attributes/default.rb
+++ cookbooks/rightscale/attributes/default.rb
@@ -3,3 +3,12 @@ default['rightscale']['monitoring']['collectd_plugins'] = [ "cpu", "df", "disk",
 # these are injected into the right_link run; defaults set for non rightscale nodes
 default['rightscale']['instance_uuid'] = false
 default['rightscale']['servers']['sketchy']['hostname'] = nil
+
+# hack/feature to override node attributes via a json provided in user-data
+if File.exist?('/var/spool/cloud/user-data.rb')
+  require 'json'
+  require '/var/spool/cloud/user-data.rb'
+  JSON.parse(ENV['RS_CHEF_JSON']).each do |key, value|
+   override[key] = value
+  end unless ENV['RS_CHEF_JSON'].nil?
+end
