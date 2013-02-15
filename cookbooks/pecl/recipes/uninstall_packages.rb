5e293e7bd8602c6acfcdaca2e5b5af2cf49b0d69
Chris Fordham <chris.fordham@rightscale.com>
2013-02-15 13:00:31 +1100

    Include pecl::install instead of default recipe.

--- cookbooks/pecl/recipes/uninstall_packages.rb
+++ cookbooks/pecl/recipes/uninstall_packages.rb
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include_recipe "pecl"
+include_recipe "pecl::install"
 
 if node['pecl']['packages_remove'].nil?
   log "No pecl packages specified for removal, skipping."
