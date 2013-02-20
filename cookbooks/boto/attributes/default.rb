36e35c2911a7f59e2888f1010e68a10be7df5e98
Chris Fordham <chris.fordham@rightscale.com>
2013-02-20 13:00:49 +1100

    Add default interpreter attribue, ebs and region attribute defaults.

--- cookbooks/boto/attributes/default.rb
+++ cookbooks/boto/attributes/default.rb
@@ -7,6 +7,14 @@ default['boto']['install_method'] = 'package'
 default['boto']['num_retries'] = 10
 default['boto']['debug'] = 0
 
+# python interpreter
+case node['platform']
+when "arch"
+  default['boto']['python']['interpreter'] = '/usr/bin/python2'
+else
+  default['boto']['python']['interpreter'] = 'python'
+end
+
 # AWS credentials
 default['boto']['aws_access_key_id'] = nil
 default['boto']['aws_secret_access_key'] = nil
@@ -14,6 +22,14 @@ default['boto']['aws_secret_access_key'] = nil
 default['boto']['pkg_filename'] = "#{node['boto']['package_prefix']}#{node['boto']['src_version']}#{node['boto']['package_extension']}"
 default['boto']['pkg_url'] = "#{node['boto']['mirror_url_prefix']}#{node['boto']['pkg_filename']}"
 
+# s3 specific   operational attributes
 default['boto']['s3_fetch_bucket'] = nil
 default['boto']['s3_fetch_file'] = nil
 default['boto']['s3_fetch_file_destination'] = nil
+
+# ebs specific operational attributes
+default['boto']['ebs']['volume']['id'] = nil
+
+# aws region defaults
+default['boto']['ec2']['region']['endpoint'] = 'us-east-1.ec2.amazonaws.com'
+default['boto']['ec2']['region']['name'] = 'us-east-1'
