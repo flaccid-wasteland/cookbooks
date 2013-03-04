83e1076676b435dffe4dee9237a7510aec5a26b6
Chris Fordham <chris@xhost.com.au>
2013-03-04 12:00:06 +1100

    Add in recent attributes and recipes to metadata.rb.

--- cookbooks/boto/metadata.rb
+++ cookbooks/boto/metadata.rb
@@ -7,23 +7,34 @@ version          "0.0.1"
 
 depends          "python"
 
-recipe "boto::default", "Installs & configures boto."
-recipe "boto::install", "Installs boto."
 recipe "boto::configure", "Configures boto."
+recipe "boto::default", "Installs & configures boto."
+recipe "boto::ebs_attach_volume", "Attaches a given EBS volume to an instance."
+recipe "boto::ebs_create_snapshot", "Creates an EBS snapshot from a given EBS volume ID."
+recipe "boto::ebs_mount_volume", "Mounts an EBS volume attached to an instance."
+recipe "boto::ebs_print_attached_volumes", "Prints all EBS volumes attached to an instance."
+recipe "boto::ebs_restore_snapshot", "Restores an EBS snapshot from a given EBS snapshot ID."
+recipe "boto::ec2_print_instance_id", "Prints the instance ID of the host instance."
+recipe "boto::ec2_print_instance_metadata", "Prints the instance metadata of the host instance in JSON format."
+recipe "boto::install", "Installs boto."
 recipe "boto::install_from_package", "Installs boto by package."
 recipe "boto::install_from_pip", "Installs boto using PIP."
 recipe "boto::install_from_source", "Installs boto from source."
 recipe "boto::s3_fetch_file", "Fetches and stores a file locally from S3."
 recipe "boto::s3_fetch_and_extract_file", "Fetches and extracts a file from S3."
 recipe "boto::s3_store_file", "Stores a local file to an S3 bucket."
-recipe "boto::ebs_create_snapshot", "Creates an EBS snapshot from a given EBS volume ID."
-recipe "boto::ebs_restore_snapshot", "Restores an EBS snapshot from a given EBS volume ID."
+
+attribute "boto/ec2/instance/id",
+  :display_name => "boto EC2 instance ID",
+  :description => "The EC2 instance ID to use for operations such as backup and restore (default is the instance ID of the host running the recipe).",
+  :required => "optional",
+  :recipes => [ "boto::ebs_create_snapshot", "boto::ebs_attach_volume", "boto::ebs_detach_volume" ]
 
 attribute "boto/ebs/volume/id",
   :display_name => "boto EBS volume ID",
   :description => "The EBS volume ID to use for operations such as backup and restore.",
   :required => "required",
-  :recipes => [ "boto::ebs_create_snapshot" ]
+  :recipes => [ "boto::ebs_create_snapshot", "boto::ebs_attach_volume", "boto::ebs_detach_volume" ]
 
 attribute "boto/ebs/volume/size",
   :display_name => "boto EBS volume size",
@@ -31,6 +42,14 @@ attribute "boto/ebs/volume/size",
   :default => "1",
   :required => "recommended",
   :recipes => [ "boto::ebs_create_snapshot", "boto::ebs_restore_snapshot" ]
+
+attribute "boto/ebs/volume/block_device",
+  :display_name => "boto EBS volume block device",
+  :description => "The EBS volume block device to use for operations such as backup and restore.",
+  :default => "/dev/sdh",
+  :required => "recommended",
+  :choice => [ '/dev/sde', '/dev/sdf', '/dev/sdg', '/dev/sdh', '/dev/sdi', '/dev/sdj', '/dev/sdk', '/dev/sdl', '/dev/sdm', '/dev/sdn', '/dev/sdo', '/dev/sdp', '/dev/sdq', '/dev/sdr', '/dev/sds', '/dev/sdt', '/dev/sdu', '/dev/sdv', '/dev/sdw', '/dev/sdx', '/dev/sdy', '/dev/sdz' ],
+  :recipes => [ "boto::ebs_restore_snapshot", "boto::ebs_attach_volume", "boto::ebs_detach_volume" ]
   
 attribute "boto/ebs/snapshot/id",
   :display_name => "boto EBS snapshot ID",
@@ -44,7 +63,7 @@ attribute "boto/ec2/region/endpoint",
   :default => "us-east-1.ec2.amazonaws.com",
   :required => "recommended",
   :choice => [ 'us-east-1.ec2.amazonaws.com', 'us-west-1.ec2.amazonaws.com', 'eu-west-1.ec2.amazonaws.com', 'ap-southeast-1.ec2.amazonaws.com' ],
-  :recipes => [ "boto::default", "boto::ebs_create_snapshot", "boto::ebs_restore_snapshot" ]
+  :recipes => [ "boto::default", "boto::ebs_create_snapshot", "boto::ebs_restore_snapshot", "boto::ebs_attach_volume", "boto::ebs_detach_volume" ]
 
 attribute "boto/ec2/region/name",
   :display_name => "boto EC2 region name",
@@ -52,7 +71,7 @@ attribute "boto/ec2/region/name",
   :default => "us-east-1",
   :required => "recommended",
   :choice => [ 'us-east-1', 'us-west-1', 'eu-west-1', 'ap-southeast-1' ],
-  :recipes => [ "boto::default", "boto::ebs_create_snapshot", "boto::ebs_restore_snapshot" ]
+  :recipes => [ "boto::default", "boto::ebs_create_snapshot", "boto::ebs_restore_snapshot", "boto::ebs_attach_volume", "boto::ebs_detach_volume" ]
 
 attribute "boto/ec2/availability_zone",
   :display_name => "boto EC2 availability zone",
