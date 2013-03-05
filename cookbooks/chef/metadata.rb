abf53a3b47cedb2daa05069489ecfd98decdf904
Chris Fordham <chris@xhost.com.au>
2013-03-05 11:25:05 +1100

    Add more attributes to metadata.

--- cookbooks/chef/metadata.rb
+++ cookbooks/chef/metadata.rb
@@ -8,9 +8,35 @@ version          "0.1.0"
 recipe           "chef::default", "Includes the chef::install recipe."
 recipe           "chef::install", "Installs Chef by the desired install method (default=omnibus)."
 
+attribute "chef/version",
+  :display_name => "Chef version",
+  :description => "The Chef version to install.",
+  :default => "10.24.0-1",
+  :recipes => [ "chef::default", "chef::install" ],
+  :choice => [ "10.16.4-1", "10.16.4-2", "10.16.6-1", "10.18.0-1", "10.18.2-1", "10.18.2-2", "10.20.0-1", "10.22.0-1", "10.24.0-1", "11.0.0-1", "11.2.0-1", "11.4.0-1" ]
+  
 attribute "chef/install_method",
   :display_name => "Chef install method",
   :description => "The method to install Chef with (omnibus or package).",
   :default => "omnibus",
-  :recipes => [ "chef::install" ],
+  :recipes => [ "chef::default", "chef::install" ],
   :choice => [ "omnibus", "package" ]
+
+attribute "chef/solo/config_file",
+  :display_name => "Chef Solo configuration file",
+  :description => "The Chef Solo configuration file aka solo.rb.",
+  :default => "/etc/chef/solo.rb",
+  :recipes => [ "chef::configure_chef_solo" ]
+  
+attribute "chef/solo/cookbook_path",
+  :display_name => "Chef Solo cookbook path",
+  :description => "The Chef Solo cookbook path for use with solo.rb.",
+  :type => 'Array',
+  :default => [ "/var/chef/cookbooks", "/var/chef/site-cookbooks" ],
+  :recipes => [ "chef::configure_chef_solo" ]
+
+attribute "chef/solo/json_attribs_file",
+  :display_name => "Chef JSON file",
+  :description => "The Chef JSON file for use with solo.rb.",
+  :default => "/etc/chef/node.json",
+  :recipes => [ "chef::configure_chef_solo" ]  
