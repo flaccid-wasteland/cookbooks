61811294d2024226ee8c54704dfd91f3962a6b25
Chris Fordham <chris@xhost.com.au>
2013-03-05 14:47:15 +1100

    Add chef/solo/json attribute and append /cookbooks for rightscale cookbook_path folders.

--- cookbooks/chef/attributes/default.rb
+++ cookbooks/chef/attributes/default.rb
@@ -1,6 +1,7 @@
 default['chef']['version'] = '10.24.0-1'
 default['chef']['install_method'] = 'omnibus'
 default['chef']['parent'] = 'rightscale'
+default['chef']['json'] = nil
 default['chef']['solo']['config_file'] = '/etc/chef/solo.rb'
 default['chef']['solo']['json_attribs_file'] = '/etc/chef/node.json'
 
@@ -8,7 +9,7 @@ if node['chef']['parent'] == 'rightscale'
   cache = '/var/cache/rightscale/cookbooks/default'
   cache_subdirs = Array.new
   Dir.chdir(cache)
-  Dir['*/'].each {|subdir| cache_subdirs.push("#{cache}/#{subdir}") }
+  Dir['*/'].each {|subdir| cache_subdirs.push("#{cache}/#{subdir}/cookbooks") }
   default['chef']['solo']['cookbook_path'] = cache_subdirs
 else
   default['chef']['solo']['cookbook_path'] = [ "/var/chef/cookbooks", "/var/chef/site-cookbooks" ]
