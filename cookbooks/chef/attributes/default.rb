f59c548ba21956c42a4141d5d06883a6322a2fa6
Chris Fordham <chris@xhost.com.au>
2013-03-05 11:25:15 +1100

    Add more default attributes.

--- cookbooks/chef/attributes/default.rb
+++ cookbooks/chef/attributes/default.rb
@@ -1,1 +1,5 @@
+default['chef']['version'] = '10.24.0-1'
 default['chef']['install_method'] = 'omnibus'
+default['chef']['solo']['config_file'] = '/etc/chef/solo.rb'
+default['chef']['solo']['cookbook_path'] = [ "/var/chef/cookbooks", "/var/chef/site-cookbooks" ]
+default['chef']['solo']['json_attribs_file'] = '/etc/chef/node.json'
