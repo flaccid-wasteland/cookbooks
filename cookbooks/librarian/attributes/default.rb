0fb62480a3a81506c7335ae08c67cb936c591a0f
Chris Fordham <chris@xhost.com.au>
2013-04-07 12:46:20 +1000

    Add default attributes.

---
+++ cookbooks/librarian/attributes/default.rb
@@ -0,0 +1,3 @@
+default['librarian']['chef']['cookbook_path'] = '/var/chef/cookbooks'
+default['librarian']['chef']['cheffile'] = nil
+#default['librarian']['chef']['cheffile'] = "https://raw.github.com/rightscale-blueprints/linux_server/master/Cheffile"
