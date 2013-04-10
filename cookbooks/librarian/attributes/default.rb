0b0d3eb6fe148294fcac134dd2332c2c3546f040
Chris Fordham <chris@xhost.com.au>
2013-04-10 13:40:22 +1000

    Add uses of gem_binary in attribute defaults.

--- cookbooks/librarian/attributes/default.rb
+++ cookbooks/librarian/attributes/default.rb
@@ -3,3 +3,10 @@ default['librarian']['chef']['cheffile'] = nil
 #default['librarian']['chef']['cheffile'] = "https://raw.github.com/rightscale-blueprints/linux_server/master/Cheffile"
 default['librarian']['install_git'] = true
 default['chef']['parent'] = 'vagrant'
+default['chef']['install_method'] = 'omnibus'
+
+if node['chef']['install_method'] == 'omnibus'
+  default['librarian']['gem_binary'] = "/opt/chef/embedded/bin/gem"
+else
+  default['librarian']['gem_binary'] = File.join(Gem.bindir, "gem")
+end
