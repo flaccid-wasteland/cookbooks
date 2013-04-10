ce0478a34e4b2c2bf8c862dba5abe0333611f036
Chris Fordham <chris@xhost.com.au>
2013-04-10 10:13:46 +1000

    Re-order attributes in default.rb.

--- cookbooks/librarian/attributes/default.rb
+++ cookbooks/librarian/attributes/default.rb
@@ -1,5 +1,5 @@
 default['librarian']['chef']['cookbook_path'] = '/var/chef/cookbooks'
 default['librarian']['chef']['cheffile'] = nil
+#default['librarian']['chef']['cheffile'] = "https://raw.github.com/rightscale-blueprints/linux_server/master/Cheffile"
 default['librarian']['install_git'] = true
 default['chef']['parent'] = 'vagrant'
-#default['librarian']['chef']['cheffile'] = "https://raw.github.com/rightscale-blueprints/linux_server/master/Cheffile"
