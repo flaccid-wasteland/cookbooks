0f7d27f1ff7ba9cfacfb3e34df0d861dee936dfa
Chris Fordham <chris@xhost.com.au>
2013-04-12 09:00:33 +1000

    Add inclusion of recipes to default recipe.

--- cookbooks/concrete5/recipes/default.rb
+++ cookbooks/concrete5/recipes/default.rb
@@ -1,4 +1,3 @@
-#
 # Cookbook Name:: concrete5
 # Recipe:: default
 #
@@ -15,4 +14,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+
+include_recipe "concrete5::install"
+
+include_recipe "concrete5::configure" unless node['concrete5']['install']['source_only']
