d3258eee8549b7e65e2d3f81b73e56660aec0425
Chris Fordham <chris@xhost.com.au>
2013-04-12 09:00:05 +1000

    Add initial install recipe.

---
+++ cookbooks/concrete5/recipes/install.rb
@@ -0,0 +1,35 @@
+# Cookbook Name:: concrete5
+# Recipe:: default
+#
+# Copyright 2013, Chris Fordham
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+directory node['concrete5']['install']['source_destination']
+
+if node['concrete5']['install']['source_only']
+  deploy_revision "#{node['concrete5']['install']['source_destination']}" do
+    repo 'git://github.com/concrete5/concrete5.git'
+    migrate false
+    #purge_before_symlink %w{one two folder/three}
+    #create_dirs_before_symlink []
+    #symlinks(
+    #  "one"   => "one",
+    #  "two"   => "two",
+    #  "three" => "folder/three"
+    #)
+    #before_restart do
+      # some Ruby code
+    #end
+  end
+end
