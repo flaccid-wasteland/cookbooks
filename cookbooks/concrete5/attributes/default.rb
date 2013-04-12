2380b460fb8b91c69ddb5777e3a8744915cc960f
Chris Fordham <chris@xhost.com.au>
2013-04-12 12:55:39 +1000

    Add concrete5/install/source_url attribute.

--- cookbooks/concrete5/attributes/default.rb
+++ cookbooks/concrete5/attributes/default.rb
@@ -1,8 +1,9 @@
 default['concrete5']['web_root'] = '/var/www'
 
+default['concrete5']['install']['git'] = true
 default['concrete5']['install']['source_only'] = true
 default['concrete5']['install']['source_destination'] = '/usr/share/concrete5'
-default['concrete5']['install']['git'] = true
+default['concrete5']['install']['source_url'] = "git://github.com/concrete5/concrete5.git"
 
 default['concrete5']['site']['name'] = 'Concrete5'
 default['concrete5']['site']['description'] = nil
