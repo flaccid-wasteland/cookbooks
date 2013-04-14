b6d20a3249c1c531dd615a7c093fcb648134dc64
Chris Fordham <chris@xhost.com.au>
2013-04-15 09:40:52 +1000

    Fix recipes option in metadata.

--- cookbooks/gem_install_test/metadata.rb
+++ cookbooks/gem_install_test/metadata.rb
@@ -11,9 +11,9 @@ recipe "gem_install_test::default", "Installs a given RubyGem by given resoure(s
 attribute "gem_install_test/gem",
   :display_name => "Gem to install",
   :description => "The RubyGem to install with the test.",
-  :recipes => [ "gem_install_test::gem" ]
+  :recipes => [ "gem_install_test::default" ]
 
 attribute "gem_install_test/methods",
   :display_name => "Gem install methods",
   :description => "The install methods to execute with the test, gem_package and/or chef_gem.",
-  :recipes => [ "gem_install_test::methods" ]
+  :recipes => [ "gem_install_test::default" ]
