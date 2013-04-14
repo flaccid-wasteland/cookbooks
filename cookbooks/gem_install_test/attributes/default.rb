e41329d232a302efa635182d2606ca278da2f564
Chris Fordham <chris@xhost.com.au>
2013-04-15 09:14:51 +1000

    Initial add of gem_install_test cookbook.

---
+++ cookbooks/gem_install_test/attributes/default.rb
@@ -0,0 +1,2 @@
+default['gem_install_test']['gem'] = 'a'
+default['gem_install_test']['methods'] = [ 'gem_package', 'chef_gem' ]
---
+++ cookbooks/gem_install_test/metadata.rb
@@ -0,0 +1,19 @@
+name             'gem_install_test'
+maintainer       'Chris Fordham'
+maintainer_email 'chris@xhost.com.au'
+license          'Apache 2.0'
+description      'Installs/Configures gem_install_test'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+recipe "gem_install_test::default", "Installs a given RubyGem by given resoure(s)."
+
+attribute "gem_install_test/gem",
+  :display_name => "Gem to install",
+  :description => "The RubyGem to install with the test.",
+  :recipes => [ "gem_install_test::gem" ]
+
+attribute "gem_install_test/methods",
+  :display_name => "Gem install methods",
+  :description => "The install methods to execute with the test, gem_package and/or chef_gem.",
+  :recipes => [ "gem_install_test::methods" ]
