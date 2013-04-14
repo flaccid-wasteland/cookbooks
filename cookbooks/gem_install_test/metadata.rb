d8d2e31ee40d3401d1fd8ac1a20432cccd9ed61f
Chris Fordham <chris@xhost.com.au>
2013-04-15 09:44:30 +1000

    Add default options to metadata attributes.

--- cookbooks/gem_install_test/metadata.rb
+++ cookbooks/gem_install_test/metadata.rb
@@ -11,9 +11,11 @@ recipe "gem_install_test::default", "Installs a given RubyGem by given resoure(s
 attribute "gem_install_test/gem",
   :display_name => "Gem to install",
   :description => "The RubyGem to install with the test.",
+  :default => "a",
   :recipes => [ "gem_install_test::default" ]
 
 attribute "gem_install_test/methods",
   :display_name => "Gem install methods",
   :description => "The install methods to execute with the test, gem_package and/or chef_gem.",
+  :default => [ "gem_package", "chef_gem" ],
   :recipes => [ "gem_install_test::default" ]
