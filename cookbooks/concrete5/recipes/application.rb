dad7993f5b2a66cdf70bf85fc27f6de3f978c69f
Chris Fordham <chris@xhost.com.au>
2013-04-16 17:27:57 +1000

    Remove accidental pastes of default assignments for pre-load block at top.

--- cookbooks/concrete5/recipes/application.rb
+++ cookbooks/concrete5/recipes/application.rb
@@ -24,11 +24,11 @@ server = node['concrete5']['db']['server']
 name = node['concrete5']['site']['name']
 description = node['concrete5']['site']['description']
 password_salt = node['concrete5']['site']['password_salt']
-production_mode = node['concrete5']['site']['production_mode'] = '0'
-admin_group = node['concrete5']['site']['admin']['group'] = 'Administrators'
-cache_library = node['concrete5']['site']['cache_library'] = 'apc'
-test_email = node['concrete5']['site']['test']['email'] = nil
-image_upload_crop_size_limit = node['concrete5']['site']['image_upload']['crop_size_limit'] = '5 * 1024 * 1024'
+production_mode = node['concrete5']['site']['production_mode']
+admin_group = node['concrete5']['site']['admin']['group']
+cache_library = node['concrete5']['site']['cache_library']
+test_email = node['concrete5']['site']['test']['email']
+image_upload_crop_size_limit = node['concrete5']['site']['image_upload']['crop_size_limit']
 
 application "#{node['concrete5']['site']['name']}" do
   path node['concrete5']['web_root']
