c1d88fb47e21b3b315c1591dbb46884a06daf22e
Chris Fordham <chris@xhost.com.au>
2013-04-16 17:14:26 +1000

    Re-factor namings of attributes.

--- cookbooks/concrete5/attributes/default.rb
+++ cookbooks/concrete5/attributes/default.rb
@@ -1,4 +1,6 @@
 default['concrete5']['web_root'] = '/var/www'
+default['concrete5']['system_user'] = 'root'
+default['concrete5']['system_group'] = 'root'
 
 default['concrete5']['install']['git'] = true
 default['concrete5']['install']['source_only'] = false
@@ -9,21 +11,16 @@ default['concrete5']['install']['revision'] = 'master'
 default['concrete5']['install']['packages'] = nil
 
 default['concrete5']['site']['name'] = 'Concrete5'
-default['concrete5']['site']['description'] = nil
+default['concrete5']['site']['description'] = 'Concrete5'
 default['concrete5']['site']['file'] = 'config/site.php'
+default['concrete5']['site']['password_salt'] = 'foobartodo'
+default['concrete5']['site']['production_mode'] = '0'
+default['concrete5']['site']['admin']['group'] = 'Administrators'
+default['concrete5']['site']['cache_library'] = 'apc'
+default['concrete5']['site']['test']['email'] = nil
+default['concrete5']['site']['image_upload']['crop_size_limit'] = '5 * 1024 * 1024'
 
 default['concrete5']['db']['server'] = nil
 default['concrete5']['db']['user'] = 'concrete5'
 default['concrete5']['db']['password'] = nil
 default['concrete5']['db']['schema'] = 'concrete5'
-
-default['concrete5']['password_salt'] = nil
-default['concrete5']['production_mode'] = '0'
-default['concrete5']['admin']['group'] = 'Administrators'
-default['concrete5']['cache_library'] = 'apc'
-default['concrete5']['test']['email'] = nil
-
-default['concrete5']['system_user'] = 'root'
-default['concrete5']['system_group'] = 'root'
-
-default['concrete5']['image_upload']['crop_size_limit'] = '5 * 1024 * 1024'
