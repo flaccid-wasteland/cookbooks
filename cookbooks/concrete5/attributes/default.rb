51e9aa8e0dbd6aff96ff56196c9d1f91f7ddd534
Chris Fordham <chris@xhost.com.au>
2013-04-22 10:50:38 +1000

    Add in all URLs for archive downloads plus more node attrs.

--- cookbooks/concrete5/attributes/default.rb
+++ cookbooks/concrete5/attributes/default.rb
@@ -2,6 +2,35 @@ default['concrete5']['web_root'] = '/var/www'
 default['concrete5']['system_user'] = 'root'
 default['concrete5']['system_group'] = 'root'
 
+default['concrete5']['archive_url']['5.6.1.2'] = "http://www.concrete5.org/download_file/-/view/51635/8497/"
+default['concrete5']['archive_url']['5.6.1.1'] = "http://www.concrete5.org/download_file/-/view/49906/8497/"
+default['concrete5']['archive_url']['5.6.1'] = "http://www.concrete5.org/download_file/-/view/49906/8497/"
+default['concrete5']['archive_url']['5.6.0.2'] = "http://www.concrete5.org/download_file/-/view/44326/8497/"
+default['concrete5']['archive_url']['5.6.0.1'] = "http://www.concrete5.org/download_file/-/view/43620/8497/"
+default['concrete5']['archive_url']['5.6.0'] = "http://www.concrete5.org/download_file/-/view/43239/8497/"
+default['concrete5']['archive_url']['5.5.2.1'] = "http://www.concrete5.org/download_file/-/view/37862/8497/"
+default['concrete5']['archive_url']['5.5.2'] = "http://www.concrete5.org/download_file/-/view/36984/8497/"
+default['concrete5']['archive_url']['5.5.1'] = "http://www.concrete5.org/download_file/-/view/33453/8497/"
+default['concrete5']['archive_url']['5.5.0'] = "http://www.concrete5.org/download_file/-/view/32086/8497/"
+default['concrete5']['archive_url']['5.4.2.2'] = "http://www.concrete5.org/download_file/-/view/29813/8497/"
+default['concrete5']['archive_url']['5.4.2.1'] = "http://www.concrete5.org/download_file/-/view/28657/8497/"
+default['concrete5']['archive_url']['5.4.2'] = "http://www.concrete5.org/download_file/-/view/27827/8497/"
+default['concrete5']['archive_url']['5.4.1.1'] = "http://www.concrete5.org/download_file/-/view/20470/8497/"
+default['concrete5']['archive_url']['5.4.1'] = "http://www.concrete5.org/download_file/-/view/19115/8497/"
+default['concrete5']['archive_url']['5.4.0.5'] = "http://www.concrete5.org/download_file/-/view/15153/8497/"
+default['concrete5']['archive_url']['5.3.3.1'] = "http://www.concrete5.org/download_file/-/view/12742/8497/"
+default['concrete5']['archive_url']['5.3.3'] = "http://www.concrete5.org/download_file/-/view/12660/8497/"
+default['concrete5']['archive_url']['5.3.2'] = "https://sourceforge.net/projects/concretecms/files/concrete5/5.3.2/concrete5.3.2.zip/download"
+default['concrete5']['archive_url']['5.3.1.1'] = "http://www.concrete5.org/download_file/-/view/12509/8497/"
+default['concrete5']['archive_url']['5.2.1'] = "http://www.concrete5.org/download_file/-/view/12011"
+default['concrete5']['archive_url']['5.1.1'] = "http://www.concrete5.org/download_file/-/view/11759"
+default['concrete5']['archive_url']['5.0.0'] = "http://www.concrete5.org/download_file/-/view/11760"
+
+# install method can be 'archive' or 'git' (bitnami or others may be possible TODO if able to be non-interactive)
+default['concrete5']['install']['method'] = 'archive'
+default['concrete5']['install']['version'] = '5.6.1.2'
+default['concrete5']['install']['archive_url'] = node['concrete5']['archive_url'][node['concrete5']['install']['version']]
+default['concrete5']['install']['sha256sum'] = 'a99cd36ed18cad757cbfc8480dadb64f0f22694226d1b57e1f8b038cc2027e14'
 default['concrete5']['install']['git'] = true
 default['concrete5']['install']['source_only'] = false
 default['concrete5']['install']['source_destination'] = '/usr/share/concrete5'
