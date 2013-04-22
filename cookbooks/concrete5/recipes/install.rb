bab4d58ccb09d80dc523ef971106b2e6c4ac3610
Chris Fordham <chris@xhost.com.au>
2013-04-22 10:55:48 +1000

    Fix node ref for install method.

--- cookbooks/concrete5/recipes/install.rb
+++ cookbooks/concrete5/recipes/install.rb
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-case node['concrete5']['install_method']
+case node['concrete5']['install']['method']
   when 'archive'
     include_recipe "concrete5::archive"
   when 'git'
