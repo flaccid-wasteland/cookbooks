7c903482eeb16dff2a0dec09fd3cbe925c2bd457
Chris Fordham <chris@xhost.com.au>
2013-05-20 10:13:33 +1000

    Add depends cookbooks to metadata.rb.

--- cookbooks/rails_app/metadata.rb
+++ cookbooks/rails_app/metadata.rb
@@ -5,3 +5,7 @@ license          'Apache 2.0'
 description      'Installs/Configures rails_app'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends "application_ruby"
+depends "git"
+depends "runit"
