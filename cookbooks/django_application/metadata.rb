3f2b94b369558f17113b53fb866f5de6593e753e
Chris Fordham <chris.fordham@rightscale.com>
2013-06-18 17:25:50 +1000

    Depend on version control cookbooks.

--- cookbooks/django_application/metadata.rb
+++ cookbooks/django_application/metadata.rb
@@ -6,3 +6,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 depends "application_python"
+depends "git"
+depends "mercurial"
+depends "subversion"
