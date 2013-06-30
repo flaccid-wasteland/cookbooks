297a1da2e25bb74ed4ff24e6a4971b91da32fb52
Chris Fordham <chris@xhost.com.au>
2013-06-30 15:22:45 +1000

    Comment out unicorn options not yet used in sub resource options collection.

--- cookbooks/rails_app/recipes/application.rb
+++ cookbooks/rails_app/recipes/application.rb
@@ -44,27 +44,27 @@ application "#{node['rails_app']['name']}" do
   case node['rails_app']['http_server']
   when "unicorn"
     unicorn do
-      after_fork node['rails_app']['unicorn']['after_fork']
-      before_exec node['rails_app']['unicorn']['before_exec']
+      #after_fork node['rails_app']['unicorn']['after_fork']          # not yet collected
+      #before_exec node['rails_app']['unicorn']['before_exec']        # not yet collected
       before_fork node['rails_app']['unicorn']['before_fork']
-      copy_on_write node['rails_app']['unicorn']['copy_on_write']
-      enable_stats node['rails_app']['unicorn']['enable_stats']
-      forked_group node['rails_app']['unicorn']['forked_group']
-      forked_user node['rails_app']['unicorn']['forked_user']
-      group node['rails_app']['unicorn']['group']
-      listen node['rails_app']['unicorn']['listen']
-      mode node['rails_app']['unicorn']['mode']
-      notifies node['rails_app']['unicorn']['notifies']
-      owner node['rails_app']['unicorn']['owner']
-      pid node['rails_app']['unicorn']['pid']
+      #copy_on_write node['rails_app']['unicorn']['copy_on_write']    # not yet collected
+      #enable_stats node['rails_app']['unicorn']['enable_stats']      # not yet collected
+      #forked_group node['rails_app']['unicorn']['forked_group']      # not yet collected
+      #forked_user node['rails_app']['unicorn']['forked_user']        # not yet collected
+      #group node['rails_app']['unicorn']['group']                    # not yet collected
+      #listen node['rails_app']['unicorn']['listen']                  # not yet collected
+      #mode node['rails_app']['unicorn']['mode']                      # not yet collected
+      #notifies node['rails_app']['unicorn']['notifies']              # not yet collected
+      #owner node['rails_app']['unicorn']['owner']                    # not yet collected
+      #pid node['rails_app']['unicorn']['pid']
       port node['rails_app']['unicorn']['port']
       preload_app node['rails_app']['unicorn']['preload_app']
-      stderr_path node['rails_app']['unicorn']['stderr_path']
-      stdout_path node['rails_app']['unicorn']['stdout_path']
+      #stderr_path node['rails_app']['unicorn']['stderr_path']        # not yet collected
+      #stdout_path node['rails_app']['unicorn']['stdout_path']
       worker_processes node['rails_app']['unicorn']['worker_processes']
       worker_timeout node['rails_app']['unicorn']['worker_timeout']
-      working_directory node['rails_app']['unicorn']['working_directory']
-      unicorn_command_line node['rails_app']['unicorn']['unicorn_command_line']
+      #working_directory node['rails_app']['unicorn']['working_directory']    # not yet collected
+      #unicorn_command_line node['rails_app']['unicorn']['unicorn_command_line']
     end
   when "apache2"
     passenger_apache2 do
