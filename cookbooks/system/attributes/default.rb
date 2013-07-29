28bcd8b2633f3687f7c1c53d56f65d03594081e9
Chris Fordham <chris@xhost.com.au>
2013-07-29 11:00:22 +1000

    Add install_packages attributes.

--- cookbooks/system/attributes/default.rb
+++ cookbooks/system/attributes/default.rb
@@ -3,3 +3,6 @@ default["system"]["short_hostname"] = 'localhost'
 default["system"]["domain"] = 'localdomain'
 default["system"]["static_hosts"] = Hash.new
 default["system"]["upgrade_packages"] = true
+
+default["system"]["packages"]["install"] = []
+default["system"]["packages"]["install_compile_time"] = []
