69e43fbe22e42fd933ba2fde56eb8d9d733ee500
Chris Fordham <chris@xhost.com.au>
2013-11-13 17:00:38 +1100

    Remove files for system cookbook due to move to xhost-cookbooks.

--- cookbooks/system/attributes/default.rb
+++
@@ -1,8 +0,0 @@
-default["system"]["timezone"] = 'UTC'
-default["system"]["short_hostname"] = 'localhost'
-default["system"]["domain"] = 'localdomain'
-default["system"]["static_hosts"] = Hash.new
-default["system"]["upgrade_packages"] = true
-
-default["system"]["packages"]["install"] = []
-default["system"]["packages"]["install_compile_time"] = []
